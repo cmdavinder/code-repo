c24a04b93755ec67b2397d43ad6bc8bea52f6e41
dinkarthakur <thakur.dinkar@gmail.com>
2016-01-14 17:34:11 +0530

    Composite service changes added

--- CompositeServices/Course-Composite-Service/src/main/java/com/wiziq/compositeservice/coursecompositeservice/controller/CourseController.java
+++ CompositeServices/Course-Composite-Service/src/main/java/com/wiziq/compositeservice/coursecompositeservice/controller/CourseController.java
@@ -1,5 +1,8 @@
 package com.wiziq.compositeservice.coursecompositeservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/create")
 public class CourseController {
-    @RequestMapping(value = "/hello")
-    public String Create(){
-        return "hello";
-    }
+
+	@Autowired
+	public Configuration con;
+	
+
+	@RequestMapping(value = "/hello")
+	public String Create() {
+
+		return "value is =" + con.getComponent();
+	}
+}
+
+@Component
+@ConfigurationProperties(prefix="info")
+class Configuration {
+	
+	private String component;
+
+	public String getComponent() {
+		return component;
+	}
+
+	public void setComponent(String component) {
+		this.component = component;
+	}
+
+	
 }
