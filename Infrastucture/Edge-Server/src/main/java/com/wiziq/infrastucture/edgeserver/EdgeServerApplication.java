99148fe19992388755d099d28b6f71e9c26326d9
dinkarthakur <thakur.dinkar@gmail.com>
2016-01-29 11:36:54 +0530

    Code for patch request added

--- Infrastucture/Edge-Server/src/main/java/com/wiziq/infrastucture/edgeserver/EdgeServerApplication.java
+++ Infrastucture/Edge-Server/src/main/java/com/wiziq/infrastucture/edgeserver/EdgeServerApplication.java
@@ -3,7 +3,11 @@ package com.wiziq.infrastucture.edgeserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.RibbonCommandFactory;
+import org.springframework.cloud.netflix.zuul.filters.route.apache.HttpClientRibbonCommandFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,5 +16,10 @@ public class EdgeServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServerApplication.class, args);
+    }
+    
+	@Bean
+    public RibbonCommandFactory<?> ribbonCommandFactory(SpringClientFactory clientFactory) {
+        return new HttpClientRibbonCommandFactory(clientFactory);
     }
 }
